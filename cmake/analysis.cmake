2d33abb0ab268d095ffcf433229c8411960547e1
Salvely <iamgwen9@gmail.com>
2024-09-12 11:49:26 +0800

    fix REMOVE_ITEM

--- cmake/analysis.cmake
+++ cmake/analysis.cmake
@@ -19,8 +19,9 @@ macro(add_analysis _target _sources)
         endforeach()
 
         # remove the /usr/include header from cppcheck_includes, 
+        message(cpp_includes: ${cppcheck_includes})
         LIST(POP_FRONT ${cppcheck_includes})
-        message(${cppcheck_includes})
+        message(cpp_includes: ${cppcheck_includes})
 
         # Add to the all target to have a high level "make analysis"
         LIST(APPEND ALL_ANALYSIS_TARGETS "${_target}_analysis")
